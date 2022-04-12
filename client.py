7252e831bf6afb38e1006850021e7310d6a0e477
plexxi <plexxious@outlook.com>
2022-04-11 21:04:11 -0500

    multiplayer is working

--- client.py
+++ client.py
@@ -1,21 +1,39 @@
 import pygame
+import pickle
+import socket
 import threading
-import sys
+from common import *
 
 running = True
 FPS = 120.0
+SERVER_ADDRESS = ("127.0.0.1", 20000)
 
 
-class Player(pygame.sprite.Sprite):
-    def __init__(self):
+class Entity(pygame.sprite.Sprite):
+    def __init__(self, color=(255, 255, 0)):
         super().__init__()
-        self.color = (255, 0, 0)
+        self.color = color
         self.image = pygame.Surface([64, 64])
         self.rect = self.image.get_rect()
         self.image.fill(self.color, self.rect)
         self.velocity = 1
 
+    def move(self, *args):
+        pass
+
+    def draw(self, display):
+        display.blit(self.image, (self.rect.x, self.rect.y))
+
+    def dump(self):
+        return self.rect.x, self.rect.y
+
+
+class Player(Entity):
+    def __init__(self):
+        super().__init__()
+
     def move(self, keys, dt):
+        pass
         x, y = 0, 0
         if keys[pygame.K_w]:
             y -= self.velocity
@@ -26,13 +44,19 @@ class Player(pygame.sprite.Sprite):
         if keys[pygame.K_d]:
             x += self.velocity
 
-        self.rect.move_ip(x * dt, y * dt)
+        self.rect.move_ip(x, y)
 
         if x or y:
             return True
 
-    def draw(self, display):
-        display.blit(self.image, (self.rect.x, self.rect.y))
+
+class NetworkedPlayer(Entity):
+    def __init__(self):
+        super().__init__((0, 255, 0))
+
+    def move(self, pos):
+        self.rect.x = pos[0]
+        self.rect.y = pos[1]
 
 
 class Game:
@@ -42,19 +66,32 @@ class Game:
         self.clock = pygame.time.Clock()
         self.running = True
         self.player = Player()
-        self.sprites = pygame.sprite.Group()
+        self.player2 = NetworkedPlayer()
         self.display = pygame.display.get_surface()
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    def handle_server(self):
+        while packet := pickle.loads(self.socket.recv(1024)):
+            self.player2.move(packet.data)
 
     def run(self):
+        self.socket.connect(SERVER_ADDRESS)
+        socket_monitor = threading.Thread(target=self.handle_server)
+        socket_monitor.start()
         while self.running:
             dt = self.clock.tick(FPS)
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     return
+
             if self.player.move(pygame.key.get_pressed(), dt):
-                self.display.fill((255, 255, 255))
-                self.player.draw(self.display)
-                pygame.display.flip()
+                packet = Packet(PacketPriority.PRIORITY, type=PacketType.MOVING, data=self.player.dump())
+                self.socket.send(pickle.dumps(packet))
+            self.display.fill((255, 255, 255))
+            self.player.draw(self.display)
+            self.player2.draw(self.display)
+            pygame.display.flip()
+        socket_monitor.join()
 
 
 if __name__ == "__main__":
--- server.py
+++ server.py
@@ -5,17 +5,18 @@ import socket
 
 SERVER_ADDR = ("127.0.0.1", 20000)
 
+
 class GameUDPServer(socketserver.ThreadingUDPServer):
     def __init__(self, server_address, request_handler_class):
         super().__init__(server_address, request_handler_class, True)
         self.clients = set()
-        self.send_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-    def broadcast(self, data):
-        print("Broadcasting data:", data.decode())
+    def broadcast(self, data, source):
         for client in self.clients:
-            print("Sending data to:", client)
-            self.send_socket.sendto(data, client)
+            if client == source:
+                continue
+            self.socket.sendto(data, client)
+
 
 class ThreadedPacketHandler(socketserver.BaseRequestHandler):
     def handle(self):
@@ -25,7 +26,8 @@ class ThreadedPacketHandler(socketserver.BaseRequestHandler):
         packet = pickle.loads(self.request[0])
         if packet.type == PacketType.DISCONNECT:
             self.server.clients.remove(self.client_address)
-        self.server.broadcast(self.request[0])
+        self.server.broadcast(self.request[0], self.client_address)
+
 
 if __name__ == "__main__":
     server = GameUDPServer(SERVER_ADDR, ThreadedPacketHandler)
